c08f6caf59df59df0489b93935999b6f8cd51b29
Michael <turner.turnermichael@gmail.com>
2016-06-09 01:23:36 -0400

    Added function PrintAmortizationSchedule

--- LoanData/LoanData.cpp
+++ LoanData/LoanData.cpp
@@ -4,10 +4,10 @@
 
 class LoanData {
 	private:
-		double Bal;
-		double n;
-		double i;
-		double A;
+		double Bal,
+			n,
+			i,
+			A;
 
 	public:
 		     LoanData(double p, double n, double e);
@@ -28,5 +28,55 @@ LoanData::LoanData(double p, double n, double e) {
 void LoanData::MakePayment(double a) {
 	Bal = (i + 1)*Bal - a;
 };
+
+//Function prints out amounts to separate txt file
+void LoanData::PrintAmortizationSchedule() {
+	double payment,
+		interestPaid,
+		principlePaid,
+		endingBalance;
+	double beginningBalance = Bal;
+
+	int length = 1, amount = (int)Bal;
+
+	if (amount > 0) {
+		for (length = 0; amount > 0; length++) {
+			amount /= 10;
+		}
+	}
+
+	length += 3;
+
+	FILE *pFile = NULL;
+	//Open the file
+	pFile = fopen("results.dat", "w");
+
+	fprintf(pFile, "Beginning Bal.   Interest paid   Principle paid   Ending Balance.\n");
+
+	while (beginningBalance > 0) {
+		if (i + 1 * Bal > A) {
+			payment = A;
+		}
+		else {
+			payment = (i + 1)*Bal;
+		}
+
+		interestPaid = i*beginningBalance;
+		principlePaid = A - interestPaid;
+		endingBalance = (i + 1)*beginningBalance - A;
+		
+		fprintf(pFile, "$%-*.2f    $%-*.2f    $%-*.2f    $%-*.2f\n",
+			length,
+			beginningBalance,
+			length,
+			interestPaid,
+			length,
+			principlePaid,
+			length,
+			endingBalance);
+
+		beginningBalance = endingBalance;
+	}
+}
 
 
