553a852dcadb471e88dc3e1b4e15d62a03cb610d
Michael <turner.turnermichael@gmail.com>
2016-06-09 01:10:25 -0400

    Added Constructor LoanData

--- LoanData/LoanData.cpp
+++ LoanData/LoanData.cpp
@@ -1,7 +1,27 @@
 #include "stdafx.h"
+#include <stdio.h>
+#include <fstream>
 
-int main()
-{
-    return 0;
-}
+class LoanData {
+	private:
+		double Bal;
+		double n;
+		double i;
+		double A;
+
+	public:
+		     LoanData(double p, double n, double e);
+		void MakePayment(double a);
+		void PrintAmortizationSchedule();
+		void PrintPayOffTimeReport(double p, double e);
+};
+
+//Constructor
+LoanData::LoanData(double p, double n, double e) {
+	i = e / 1200;
+	n = n * 12;
+	A = p*((i*pow(1 + i, n)) - 1);
+	Bal = p;
+};
+
 
